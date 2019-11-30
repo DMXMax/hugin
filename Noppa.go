9381da4bffe968e9ecaa2413765e8fd1304f9807
DMXMax <29312988+DMXMax@users.noreply.github.com>
2019-11-30 20:14:39 +0000

    update bot

--- Noppa.go
+++ Noppa.go
@@ -3,26 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
+	 "math"
 	"os"
 	"os/signal"
-	"syscall"
 	"strings"
+	"syscall"
 
+	owm "github.com/briandowns/openweathermap"
 	"github.com/bwmarrin/discordgo"
-	"github.com/ramsgoli/openweathermap"
 )
 
 // Variables used for command line parameters
 var (
-	Token string
-	weatherOnly *bool
+	DiscordToken string
+	WeatherToken string
+	weatherOnly  *bool
 )
 
 func init() {
 
-//	flag.StringVar(&Token, "t", "", "Bot Token")
-//	flag.Parse()
-	weatherOnly = flag.Bool("weather",false,"Test Weather Only")
+	//	flag.StringVar(&Token, "t", "", "Bot Token")
+	//	flag.Parse()
+	weatherOnly = flag.Bool("weather", false, "Test Weather Only")
 	flag.Parse()
 }
 
@@ -30,17 +32,31 @@ func main() {
 
 	// Create a new Discord session using the provided bot token.
 	//dg, err := discordgo.New("Bot " + Token)
-	if Token = os.Getenv("NOPPA_PIE"); Token == ""{
+	if WeatherToken = os.Getenv("NOPPA_WEATHER"); WeatherToken == "" {
+		fmt.Println("No Weather Token")
+		os.Exit(1)
+	}
+
+	if DiscordToken = os.Getenv("NOPPA_PIE"); DiscordToken == "" {
 		fmt.Println("No token")
 		os.Exit(1)
 	}
 
-	if *weatherOnly==true {
+	if *weatherOnly == true {
 		fmt.Println("Testing Weather Only")
+		w, err := owm.NewCurrent("C", "EN", WeatherToken)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			w.CurrentByID(4990729)
+			fmt.Println(w)
+			fmt.Printf("The current temperature in %s is %v degrees\n", w.Name, w.Main.Temp)
+			fmt.Println(w.Weather[0].Description)
+		}
 		os.Exit(0)
 	}
 
-	dg, err := discordgo.New("Bot " + Token)
+	dg, err := discordgo.New("Bot " + DiscordToken)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
@@ -76,12 +92,49 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 	// If the message is "ping" reply with "Pong!"
 	if strings.ToLower(m.Content) == "!weather" {
-		s.ChannelMessageSend(m.ChannelID, `WCOC Weather: Mostly cold with a chance of light flurries.
-	The current temperature is 4 degrees Celcius, 39 degrees Farenheit.`)
+		//s.ChannelMessageSend(m.ChannelID, getWeather(4990729))
+		HandleWeatherRequest(s,m)
 	}
 
 	// If the message is "pong" reply with "Ping!"
 	if strings.ToLower(m.Content) == "pong" {
 		s.ChannelMessageSend(m.ChannelID, "Ping!")
 	}
+}
+
+func HandleWeatherRequest(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	type City struct {
+		name  string
+		locID int
+	}
+
+	var cities = []City{City{"Millennium City", 4990729}, City{"Windsor, Ontario", 6182959}}
+
+	for _,c := range cities {
+
+		w, err := owm.NewCurrent("C", "EN", WeatherToken)
+
+		if err != nil {
+			fmt.Println(err)
+			s.ChannelMessageSend(m.ChannelID,"I cannot get the weather right now.")
+		} else {
+			s.ChannelMessageSend(m.ChannelID,"WCOC Action Weather:\n")
+			w.CurrentByID(c.locID)
+			fmt.Println(w)
+			fmt.Printf("The current temperature in %s is %v degrees\n", w.Name, w.Main.Temp)
+			fmt.Println(len(w.Weather))
+			fmt.Println(w.Weather[0].Description)
+		
+		s.ChannelMessageSend(m.ChannelID, 
+		fmt.Sprintf("For %v: %v.\nThe current temperature is %v degrees Celcius, "+
+			"%v degrees Farenheit.",
+			c.name,
+			w.Weather[0].Description,
+			math.Round(w.Main.Temp),
+			math.Round((w.Main.Temp * 1.8) + 32)))
+	
+	
+	}
+}
 }
