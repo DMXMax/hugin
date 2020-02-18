79f9ab66b0ef2268f5b1b0905b12fe5f6fa6b7c8
DMXMax <29312988+DMXMax@users.noreply.github.com>
2020-02-18 02:42:04 +0000

    Updated to use modules and datastore

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,21 +1,30 @@
 package dictionary
 
 import (
-	"database/sql"
-	"fmt"
-	"github.com/DMXMax/noppa/hugindb"
+	_ "database/sql"
+	 "fmt"
+	_ "github.com/DMXMax/noppa/hugindb"
 	dg "github.com/bwmarrin/discordgo"
 	"log"
+	"cloud.google.com/go/datastore"
+	"google.golang.org/api/iterator"
+	"context"
 )
 
-type Definition struct {
+type NewDef struct {
+	Word string
+	Display string
+	Definition string 
+}
+
+/*type Definition struct {
 	acronym    string
 	definition string
 	adv_flag   bool
 	adv_url    sql.NullString
 	adv_image  sql.NullString
 	adv_text   sql.NullString
-}
+}*/
 
 const qry = "SELECT acronym, definition, adv_flag, adv_url, adv_image, " +
 	"adv_text FROM TLA WHERE idx = ?"
@@ -23,8 +32,38 @@ const qry = "SELECT acronym, definition, adv_flag, adv_url, adv_image, " +
 func HandleDictionaryRequest(words []string, s *dg.Session,
 	m *dg.MessageCreate) {
 	var (
-		def Definition
+	//	def Definition
 	)
+	ctx := context.Background()
+
+
+	if client, err := datastore.NewClient(ctx, "hugin-00001");err == nil{
+		log.Println("Got a client")
+		q := datastore.NewQuery("dictEntry").Filter("word =", words[0])
+		t := client.Run(ctx, q)
+		def :=  NewDef{}
+		if key,err := t.Next(&def); err == nil{
+			s.ChannelMessageSend(m.ChannelID,
+			  fmt.Sprintf("%s: %s", def.Display, def.Definition))
+			  log.Printf("%s: %s, [%s]\n", def.Display, def.Definition, key)
+		  } else {
+	            if err == iterator.Done {
+			    s.ChannelMessageSend(m.ChannelID,
+			    fmt.Sprintf("Don't know what %v is.", words[0]))
+		    } else {
+			  log.Println("Error getting next." + err.Error())
+		  }
+	  }
+  	}else{
+		  log.Printf("no client" + err.Error())
+	  }
+
+
+	/*} else {
+		log.Printf("Error getting client %s\n", err.Error()
+	}
+
+
 	if len(words) > 0 {
 		err := hugindb.DB.QueryRow(qry, words[0]).Scan(&def.acronym,
 			&def.definition, &def.adv_flag, &def.adv_url,
@@ -44,10 +83,10 @@ func HandleDictionaryRequest(words []string, s *dg.Session,
 		}
 	} else {
 		s.ChannelMessageSend(m.ChannelID, "What do you want to know? Type !def <word>")
-	}
+	}*/
 }
 
-func ComplexDefSend(s *dg.Session, m *dg.MessageCreate, def *Definition) {
+/*func ComplexDefSend(s *dg.Session, m *dg.MessageCreate, def *Definition) {
 
 	embed := dg.MessageEmbed{
 		Title:       def.acronym,
@@ -73,4 +112,4 @@ func ComplexDefSend(s *dg.Session, m *dg.MessageCreate, def *Definition) {
 	if _, err := s.ChannelMessageSendComplex(m.ChannelID, &msgSend); err != nil {
 		log.Printf("Error %v\n", err)
 	}
-}
+}*/
